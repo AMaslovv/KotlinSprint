179f1919c735dd0fd307124c59549bd3fc829f54
AMaslov <datacard9k@gmail.com>
2023-05-14 00:07:22 +0300

    KS-1-1 variables for orders and notification after buying

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -3,10 +3,6 @@ package lesson_1
 /** by AMaslov  */
 
 fun main() {
-
-    var numberOrders : Int = 75
-    val notifThanks : String = "Благодарим Вас за покупку!"
-
-
-
+    var numberOrders: Int = 75
+    val notifThanks: String = "Благодарим Вас за покупку!"
 }
