413eb40da980cca71d74296352b38dcef9ff1718
AMaslov <datacard9k@gmail.com>
2023-05-14 13:15:01 +0300

    KS-1-4 adding variables and print in console

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,21 @@
+package lesson_1
+
+/** by AMaslov  */
+
+fun main() {
+    //Объявление переменных
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    //выводим в консоль
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
