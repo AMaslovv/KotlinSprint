4c6635cd7f2d808360d2cabfa44245bcd0690646
AMaslov <datacard9k@gmail.com>
2023-05-14 12:38:36 +0300

    KS-1-3 adding variables year, hour, minute and print in console

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_1
+
+/** by AMaslov  */
+
+fun main() {
+    //Объявление переменных
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    //вывод в консоль
+    println(year)
+    println(hour)
+    println(minute)
+
+    //запись в переменные hour и minute время посадки и вывод
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
+}
