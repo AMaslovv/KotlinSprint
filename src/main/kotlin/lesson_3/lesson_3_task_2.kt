18f4248f9aef8feb5016b8187501ae637bb8bcfb
AMaslovv <datacard9k@gmail.com>
2023-05-15 16:59:25 +0300

    KS-3-2 printout in console data of girl before married and after

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_3
+
+/** by AMaslov  */
+
+fun main() {
+    //определение и инициализация переменных ФИО и возраст
+    val ageBeforeMarried = "20"
+    val ageMarried = "22"
+    val firstName = "Татьяна"
+    val lastName = "Андреева"
+    val patrName = "Сергеевна"
+    val lastNameMarried = "Сидорова"
+
+    //Выводим в консоль данные до замужества и после
+    println("$lastName $firstName $patrName,$ageBeforeMarried")
+    println("$lastNameMarried $firstName $patrName,$ageMarried")
+}
