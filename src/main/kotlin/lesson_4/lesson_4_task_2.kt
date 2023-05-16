6bba70a5e9aadf924ac4b6683a937b04b0cee0af
AMaslov <datacard9k@gmail.com>
2023-05-17 01:29:05 +0300

    KS-4-2 print in console result checks cargo 1 and cargo 2 on limits average

---
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -0,0 +1,18 @@
+package lesson_4
+
+/** by AMaslov  */
+
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
+fun main() {
+    //инициализация переменных груз 1 и 2
+    val weightCargo1 = 20
+    val volumeCargo1 = 80
+    val weightCargo2 = 50
+    val volumeCargo2 = 100
+
+    //сравнение и printout in console
+    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${(weightCargo1 >= MIN_WEIGHT_AVERAGE && weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)}")
+    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${(weightCargo2 >= MIN_WEIGHT_AVERAGE && weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)}")
+}
