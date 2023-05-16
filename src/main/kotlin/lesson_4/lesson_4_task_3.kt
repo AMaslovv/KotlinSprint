ed365aac84aec9f382fbdaacc0b003a19d5f13a3
AMaslov <datacard9k@gmail.com>
2023-05-17 02:07:52 +0300

    KS-4-3 checking conditions for growing beans

---
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -0,0 +1,19 @@
+package lesson_4
+
+/** by AMaslov  */
+
+const val WEATHER_FOR_BEANS = "солнечная погода"
+const val STATUS_TENT = "тент раскрыт"
+const val HUMIDITY_PERCENT = "влажность 20"
+const val SEASONS = "не зима"
+
+fun main() {
+    //инициализация переменных
+    val weatherToday = "солнечная погода"
+    val statTentToday = "тент раскрыт"
+    val humidityToday = "влажность 20"
+    val seasonsNow = "зима"
+
+    //проверяем совпадают ли условия и пишем в консоль
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherToday == WEATHER_FOR_BEANS) && (statTentToday == STATUS_TENT) && (humidityToday == HUMIDITY_PERCENT) && (seasonsNow == SEASONS)}")
+}
