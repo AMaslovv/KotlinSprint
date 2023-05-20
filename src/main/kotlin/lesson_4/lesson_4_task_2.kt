66d120e9bd47d1d65dfc7bae0b5b9faf3bbbf720
AMaslov <datacard9k@gmail.com>
2023-05-20 18:00:31 +0300

    KS-4-2 print in console result checks cargo 1 and cargo 2 on limits

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -12,7 +12,15 @@ fun main() {
     val weightCargo2 = 50
     val volumeCargo2 = 100
 
-    //сравнение и printout in console
-    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${(weightCargo1 >= MIN_WEIGHT_AVERAGE && weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)}")
-    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${(weightCargo2 >= MIN_WEIGHT_AVERAGE && weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)}")
+    //проверка условия
+    val isCargoOneAverage = (weightCargo1 >= MIN_WEIGHT_AVERAGE && weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)
+    val isCargoTwoAverage = (weightCargo2 >= MIN_WEIGHT_AVERAGE && weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)
+
+
+//    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${(weightCargo1 >= MIN_WEIGHT_AVERAGE && weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)}")
+//    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${(weightCargo2 >= MIN_WEIGHT_AVERAGE && weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)}")
+
+    //printout in console
+    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: $isCargoOneAverage")
+    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: $isCargoTwoAverage")
 }
