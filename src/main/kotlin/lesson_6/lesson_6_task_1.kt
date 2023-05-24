455c7f846586b4f32909fb45e20f2c6fa165806a
AMaslov <datacard9k@gmail.com>
2023-05-25 00:19:24 +0300

    KS-6-1 ADVANCED LOGIN PASS AUTHORIZATION WITH CYCLE

---
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -0,0 +1,32 @@
+package lesson_6
+
+/** by AMaslov  */
+
+fun main() {
+    //создание переменных для логина и пароля
+    val registrUserLogin: String
+    val registrUserPass: String
+
+    println("Для авторизации необходимо создать учетную запись.\n")
+
+    //записываем в переменные логин и пароль
+    println("Введите логин для регистрации: ")
+    registrUserLogin = readln()
+    println("Придумайте пароль: ")
+    registrUserPass = readln()
+    println("Учетная запись создана.")
+
+    //в цикле проверяем данные для авторизации
+    while (true){
+        println("Введите логин: ")
+        val authorizUserLogin = readln()
+        println("Введите пароль: ")
+        val authorizUserPass = readln()
+
+        if (authorizUserLogin == registrUserLogin && authorizUserPass == registrUserPass){
+            println("Авторизация прошла успешно.")
+            break
+        }
+        else println("Неверные логин или пароль.")
+    }
+}
